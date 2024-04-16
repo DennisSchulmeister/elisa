1cd868af6ed00022548e4c5064f92b367d0d41e2
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-04-16 18:17:30 +0200

    CLI: Login screen (early wip)

--- elisa/views/login/login.go
+++ elisa/views/login/login.go
@@ -9,22 +9,56 @@
 package login
 
 import (
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+// TODO: Welcome message
+// TODO: Input field for server address (pre-filled)
+// TODO: Input field for course password
+// TODO: Login button
+// TODO: Exit button
+
+// TODO: Read elisa.yml file at startup, show warning if not found
+// TODO: Send qRPC request to server to check credentials
+
 // Initial screen after program start-up. This reads the exercise description from
-// the file `elisa.yaml` and presents an input field where the course  password can
+// the file `elisa.yaml` and presents an input field where the course password can
 // be entered to login on the server.
-type LoginModel struct {}
+type LoginModel struct {
+	welcomeMessage string
+	ServerAddress  textinput.Model
+	CoursePassword textinput.Model
+	cursorMode     cursor.Mode
+}
 
 // Create new instance of the model to read the exercise and login on the server
 func NewLoginModel() LoginModel {
-	return LoginModel{}
+	return LoginModel{
+		welcomeMessage: "Willkommen bei Elisa, der KI-Programmiertutorin für Studierende",
+		ServerAddress: textinput.Model{
+			Placeholder: "Server-Adresse",
+			CharLimit:   255,
+			// Cursor.Style:
+			// PromptStype:
+			// TextStyle:
+		},
+		CoursePassword: textinput.Model{
+			Placeholder:   "Kurs-Passwort",
+			CharLimit:     255,
+			EchoMode:      textinput.EchoPassword,
+			EchoCharacter: '•',
+			// Cursor.Style:
+			// PromptStype:
+			// TextStyle:
+		},
+	}
 }
 
 // Run initial command
 func (m LoginModel) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 // Process event messages
@@ -36,12 +70,3 @@ func (m LoginModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m LoginModel) View() string {
 	return "Login"
 }
-
-// TODO: Welcome message
-// TODO: Input field for server address (pre-filled)
-// TODO: Input field for course password
-// TODO: Login button
-// TODO: Exit button
-
-// TODO: Read elisa.yml file at startup, show warning if not found
-// TODO: Send qRPC request to server to check credentials
